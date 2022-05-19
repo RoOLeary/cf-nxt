a0db622e739e93952be573c82cd84f7b6bcf5d71
ronan <ronan@thenextweb.com>
2022-05-19 20:30:25 +0200

    let us look

--- apollo-client.js
+++ apollo-client.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://cities.local.tnw.dev/api",
+    uri: "https://thenextweb.com/dublin/api",
     cache: new InMemoryCache(),
 });
 
