7722d1d5f7a291906e3d84ebf539d2519221aaf7
ronan <ronan@thenextweb.com>
2022-06-20 23:14:11 +0200

    try again. pipelines is a bag of dicks

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'https://cities.local.tnw.dev/api',
+    uri: 'https://cities.thenextweb.com/',
     cache: new InMemoryCache(),
     ssrMode: true,
     assumeImmutableResults: true,
