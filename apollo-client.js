2a27bce81eeb519e5355479002bfc8a99e7d5f7c
ronan <ronan@thenextweb.com>
2022-06-27 09:12:04 +0200

    update apollo-client.js to pull from production url

--- apollo-client.js
+++ apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const token = 'xmhW_1_0oLV29dcV7aQsNb_LdygNi5s6';
 const client = new ApolloClient({
     
-    uri: 'https://cities.local.tnw.dev/api/',
+    uri: 'https://cities.thenextweb.com/api/',
     ssrMode: typeof window === "undefined",
     cache: new InMemoryCache(),
     credentials: 'same-origin',
@@ -11,10 +11,8 @@ const client = new ApolloClient({
       method:'POST'
     },
     headers: {
-      'Access-Control-Allow-Credentials': true,
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    
+      "Access-Control-Allow-Origin" : "*", 
+      "Access-Control-Allow-Credentials" : true
     } 
 });
 
