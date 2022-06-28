7b96152d861128594d858954d889ef9ddc5c83f3
ronan <ronan@thenextweb.com>
2022-06-28 17:07:36 +0200

    update prototype

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,64 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useRef, useEffect } from 'react'
+import styles from './../styles/Nav.module.css'
 
 export default function Nav(){
+    // const { data: session } = useSession();
+
+    // console.log(session);
+    let menuRef = useRef();
+    let unitRef = useRef();
+
+    const toggleMobileMenu = (e) => {
+        e.currentTarget.classList.toggle('mobile-menu-active');
+        menuRef.current.classList.toggle('show');
+    }
+
+    const update = (height) => {
+        const num = window.scrollY / height
+        const multiplier = Math.min(Math.max(num, 0), 1)
+        unitRef.current.style.setProperty('--multiplier', multiplier)
+    }
+
+    useEffect(() => {
+        let height = unitRef.current.offsetHeight;
+        window.addEventListener('scroll', () => update(height)); 
+    }, []);
+
     return(
-        <>
-            <ul>
-                <li><Link href={`/all-components`}><a>All Comps</a></Link></li>
-                <li><Link href={`/about`}><a>About</a></Link></li>
-                <li><Link href={`/agenda`}><a>Agenda</a></Link></li>
-                <li><Link href={`/partners`}><a>Partners</a></Link></li>
-                <li><Link href={`/faq`}><a>Faq</a></Link></li>
-                <li><Link href={`/`}><a>Home</a></Link></li>
-            </ul>
-        </>
+        <nav className={'c-nav js-nav t-dark'} ref={unitRef}>
+            <div className={'c-nav__left'}>
+                <Link href={`/`}>
+                    <a className={"c-nav__logoLink"}>
+                        <img alt="TNW Dublin" className="c-nav__logo" src="/assets/img/ft-tnw-dublin.svg" />
+                    </a>
+                </Link>
+            </div>
+            <label className={'c-nav__mobileMenuToggle'} htmlFor="navMobileMenuToggle" onClick={toggleMobileMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </label>
+            <div className="c-nav__mobileMenu c-nav__right" ref={menuRef}>
+                <ul className="c-nav__menu">
+                    <li className="c-nav__menuItem"><Link href={"/"}><a className="c-nav__menuLink">Home</a></Link></li>
+                    <li className="c-nav__menuItem"><Link href={"/about"}><a className="c-nav__menuLink">About</a></Link></li>
+                    <li className="c-nav__menuItem"><Link href={"/tech"}><a className="c-nav__menuLink">Tech</a></Link></li>
+                    <li className="c-nav__menuItem"><Link href={"/test"}><a className="c-nav__menuLink">Test</a></Link></li>
+                    <li className="c-nav__menuItem"><Link href={"/retest"}><a className="c-nav__menuLink">Retest</a></Link></li>
+                    {/* {session ? <li className="c-nav__menuItem"><Link href={"/members"}><a className="c-nav__menuLink">Members</a></Link></li> : ''}
+                    <li className="c-nav__menuItem">
+                        {!session ? <>
+                        <a className="c-button c-button--primary" onClick={signIn}>Sign In</a></>
+                        :
+                        <>
+                        <a className="c-button c-button--primary" onClick={signOut}>Sign Out</a>
+                        </>}
+                    </li> */}
+                    {/* <li className="c-nav__menuItem"><a className="c-button c-button--primary">Login</a></li> */}
+                </ul>
+            </div>
+        </nav>
     )
 }
